bbfda7e7f7be0bb48428c074a99c5fab508f63e8
Recillah Khamala <recillahk@gmail.com>
2026-07-28 22:28:35 +0300

    Add columns to shops migration: name, location, shop_type

--- db/migrate/20260728192034_create_shops.rb
+++ db/migrate/20260728192034_create_shops.rb
@@ -1,6 +1,9 @@
 class CreateShops < ActiveRecord::Migration[8.0]
   def change
     create_table :shops, id: :uuid do |t|
+      t.string :name, null: false
+      t.string :location
+      t.string :shop_type, default: 'cereal', null: false
       t.timestamps
     end
   end
