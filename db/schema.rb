81f1009fd518b2c40979ba0ec461d77e8cbd117b
Recillah Khamala <recillahk@gmail.com>
2026-07-28 23:10:58 +0300

    Update schema.rb after running customers migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_28_192034) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_28_195459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
+
+  create_table "customers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "shop_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_customers_on_shop_id"
+  end
 
   create_table "shops", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
@@ -22,4 +30,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_28_192034) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "customers", "shops"
 end
