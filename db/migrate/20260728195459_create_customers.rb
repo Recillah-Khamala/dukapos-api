e4eda1c546af01563df4c3d81edd3fca55ca95fe
Recillah Khamala <recillahk@gmail.com>
2026-07-28 23:03:59 +0300

    Generate customers migration with shop foreign key and name column

--- db/migrate/20260728195459_create_customers.rb
+++ db/migrate/20260728195459_create_customers.rb
@@ -1,6 +1,8 @@
 class CreateCustomers < ActiveRecord::Migration[8.0]
   def change
     create_table :customers, id: :uuid do |t|
+      t.references :shop, type: :uuid, null: false, foreign_key: true
+      t.string :name, null: false
       t.timestamps
     end
   end
