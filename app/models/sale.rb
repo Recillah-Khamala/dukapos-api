40699ed77b5fd0276c72c671c90bb6f5dec66396
Recillah Khamala <recillahk@gmail.com>
2026-07-29 07:42:49 +0300

    Add associations and validations to Sale and Shop models

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,9 @@
 class Sale < ApplicationRecord
+  belongs_to :shop
+
+  VALID_PAYMENT_METHODS = %w[cash mpesa credit].freeze
+
+  validates :total, presence: true, numericality: { greater_than: 0 }
+  validates :payment_method, inclusion: { in: VALID_PAYMENT_METHODS }
+  validates :completed_at, presence: true
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,6 @@
 class Shop < ApplicationRecord
   has_many :customers
   has_many :inventory_items
+  has_many :sales
   validates :name, presence: true
 end
