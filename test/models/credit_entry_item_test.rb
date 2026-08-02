1f2c9c81ea996a342f070fb5a3ec56a5efd6d71f
Recillah Khamala <recillahk@gmail.com>
2026-08-02 21:43:44 +0300

    Delete all test files now tiht the addition og rspec for testing.

--- test/models/credit_entry_item_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CreditEntryItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/credit_entry_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CreditEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
